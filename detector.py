5b2843facda58449bea5b67a1a3fafe2421852f8
io53 <eliaseberg@gmail.com>
2018-06-15 09:13:43 +0300

    fixed bugs that caused bad predictions

--- detector.py
+++ detector.py
@@ -7,12 +7,12 @@ from keras.preprocessing.image import ImageDataGenerator
  
 class Detector:
     def __init__(self):
-        self.image_size = 150
         self.model = load_model('model.h5')
+        self.image_size = self.model.input_shape[1]
+        print("image size: " + str(self.image_size))
         test_datagen = ImageDataGenerator(rescale = 1./255)
         training_set = test_datagen.flow_from_directory('dataset/validate',
                                                         target_size = (self.image_size , self.image_size),
-                                                        batch_size = 6,
                                                         class_mode = 'categorical')
         self.labels = training_set.class_indices
         print(self.labels)
@@ -20,13 +20,7 @@ class Detector:
     def detect(self, imagePath): 
         test_image = image.load_img(imagePath, target_size = (self.image_size, self.image_size))
         test_image = image.img_to_array(test_image)
+        test_image = test_image/255.
         test_image = np.expand_dims(test_image, axis = 0)
-        result = self.model.predict(test_image)
-        found = ""
-        for i, res in enumerate(result[0]):
-            if res == 1:
-                if found != "":
-                    found += ","
-                found += list(self.labels.keys())[i]
-        print(result[0])
-        return None if found == "" else found
+        result = self.model.predict_classes(test_image)
+        return list(self.labels.keys())[result[0]];
--- train.py
+++ train.py
@@ -1,48 +1,62 @@
 from keras.models import Sequential
 from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
+from keras.layers import AveragePooling2D
+from keras.layers import Dropout
 from keras.layers import Flatten
 from keras.layers import Dense
+from keras.layers import BatchNormalization
+from keras import optimizers
 from keras.preprocessing.image import ImageDataGenerator
 
-image_size = 150
+image_size = 64
 
-train_datagen = ImageDataGenerator(rescale = 1./255,
-                                   shear_range = 0.2,
-                                   zoom_range = 0.2,
-                                   horizontal_flip = True)
+train_datagen = ImageDataGenerator(
+                                   rescale = 1./255,
+                                   #shear_range = 0.2,
+                                   #zoom_range = 0.2,
+                                   #rotation_range=100, 
+                                   #width_shift_range=0.1,
+                                   #height_shift_range=0.1, 
+                                   #fill_mode='nearest',
+                                   #vertical_flip = True,
+                                   #horizontal_flip = True
+                                   )
 
 test_datagen = ImageDataGenerator(rescale = 1./255)
 
 training_set = train_datagen.flow_from_directory('dataset/train',
+                                                 shuffle = True,
                                                  target_size = (image_size, image_size),
-                                                 batch_size = 6,
                                                  class_mode = 'categorical')
 
 test_set = test_datagen.flow_from_directory('dataset/validate',
                                             target_size = (image_size, image_size),
-                                            batch_size = 6,
                                             class_mode = 'categorical')
 
 labels = test_set.class_indices
 
 model = Sequential()
 
-model.add(Conv2D(32, (3, 3), input_shape = (image_size, image_size, 3), activation = 'relu'))
+model.add(Conv2D(16, (3, 3), input_shape = (image_size, image_size, 3), activation = 'relu'))
 model.add(MaxPooling2D(pool_size = (2, 2)))
-model.add(Conv2D(32, (3, 3), activation = 'relu'))
-model.add(MaxPooling2D(pool_size = (2, 2)))
+model.add(Dropout(0.25))
+model.add(Conv2D(16, (3, 3), activation = 'relu'))
+model.add(AveragePooling2D(pool_size = (2, 2)))
+model.add(Dropout(0.25))
 model.add(Flatten())
-model.add(Dense(units = 128, activation = 'relu'))
-model.add(Dense(units = len(labels), activation = 'sigmoid'))
+model.add(Dense(units = 32, activation = 'relu'))
+model.add(Dropout(0.5))
+model.add(Dense(units = len(labels), activation = 'softmax'))
 
-model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+sgd = optimizers.SGD(lr=0.01, nesterov=True, decay=1e-6, momentum=0.9)
+history = model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+#model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
 
 model.fit_generator(training_set,
-                         steps_per_epoch = 200,
-                         epochs = 4,
+                         epochs = 50,
+                         shuffle = True,
                          validation_data = test_set,
-                         validation_steps = 10,
                          workers=16, 
                          use_multiprocessing=True
                          )
